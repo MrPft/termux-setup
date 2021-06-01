40d460787e582b46c8f226f3568b1a05b6eb4885
MrPft <85167759+MrPft@users.noreply.github.com>
2021-06-01 16:51:42 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+apt update && apt upgrade
+apt install proot-distro 
+proot-distro install ubuntu-20.04
+proot-distro login ubuntu-20.04
+apt update && apt upgrade
+apt install git python3 python3-pip curl wget sudo 
+
+
